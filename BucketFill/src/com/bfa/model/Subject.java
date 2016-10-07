9ac0f97d370ca2ef38a5c9da8aaa683e40469864
maheshhp <hpmahesh73@gmail.com>
2016-10-07 20:06:20 +0530

    Added Subject Queries

--- BucketFill/src/com/bfa/model/Subject.java
+++ BucketFill/src/com/bfa/model/Subject.java
@@ -76,9 +76,7 @@ public class Subject {
     public static ArrayList<Subject> getAllDetails(){
         try{
             
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection myConnection=DriverManager.getConnection("jdbc:mysql://localhost:8889/bucketfill","root","root");
-            
+            Connection myConnection = DBConnection.createConnection();
             Statement myStatement = myConnection.createStatement();
             String simpleQuery = "select * from Subject";
             ResultSet rs = myStatement.executeQuery(simpleQuery);            
@@ -109,8 +107,7 @@ public class Subject {
     public static ArrayList<Subject> getDetailsByCourseCode(String courseCode){
         try{
             
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection myConnection=DriverManager.getConnection("jdbc:mysql://localhost:8889/bucketfill","root","root");
+            Connection myConnection = DBConnection.createConnection();
             
             PreparedStatement myPreStatement = myConnection.prepareStatement("SELECT * FROM Subject WHERE CourseCode = ?");
             myPreStatement.setString(1, courseCode);
@@ -137,6 +134,61 @@ public class Subject {
         }
         catch(Exception e){
             System.out.println(e+" Occured in get details by id");
+            return null;
+        }
+    }
+    
+    public static ArrayList<Subject> getSubjectByYear(int year){
+        
+        Connection myConnection = DBConnection.createConnection();
+        ResultSet rs = null;
+        
+        try{
+            
+            PreparedStatement myPreStatement = myConnection.prepareStatement("SELECT * FROM Subject WHERE CourseCode LIKE ? OR ?");
+            switch(year)
+            {
+                case 2: myPreStatement.setString(1, "%3%");
+                        myPreStatement.setString(2, "%4%");
+                        rs = myPreStatement.executeQuery();
+                        break;
+                        
+                case 3: myPreStatement.setString(1, "%5%");
+                        myPreStatement.setString(2, "%6%");
+                        rs = myPreStatement.executeQuery();
+                        break;
+                        
+                case 4: myPreStatement.setString(1, "%7%");
+                        myPreStatement.setString(2, "%8%");
+                        rs = myPreStatement.executeQuery();
+                        break;
+                        
+                default: myPreStatement.setString(1, "%");
+                         myPreStatement.setString(2, "%");
+                         rs = myPreStatement.executeQuery();
+            }
+            ArrayList<Subject> subjectList = new ArrayList<>(); 
+            
+            for(int i=0;rs.next();i++)
+            {
+                Subject temp = new Subject();
+                
+                temp.courseCode = rs.getString(1);
+                temp.name = rs.getString(2);
+                temp.theory = rs.getInt(3);
+                temp.lab = rs.getBoolean(4);
+                temp.tutorial = rs.getBoolean(5);
+                temp.elective = rs.getBoolean(6);
+                temp.selfStudy = rs.getBoolean(7);
+                
+                subjectList.add(temp);
+            }
+            myConnection.close();
+            return subjectList;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in getSubjectDetailsByYear");
             return null;
         }
     }
