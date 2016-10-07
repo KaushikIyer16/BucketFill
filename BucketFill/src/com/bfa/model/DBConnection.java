a35239337d6de96dfb9807e7c76f6072ca811cc8
maheshhp <hpmahesh73@gmail.com>
2016-10-07 20:07:05 +0530

    Added DBConnection class

---
+++ BucketFill/src/com/bfa/model/DBConnection.java
@@ -0,0 +1,26 @@
+package com.bfa.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author mahesh
+ */
+public class DBConnection {
+    public static Connection createConnection()
+    {
+        Connection myConnection;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:8889/bucketfill", "root", "root");
+            return myConnection;
+        } 
+        catch(Exception e)
+        {
+            System.out.println("Error in connection class "+e);
+            return null;
+        }
+    }
+}
