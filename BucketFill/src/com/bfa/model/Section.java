38b176bdb4b0d734d8e99fef8a134f4108466fe1
maheshhp <hpmahesh73@gmail.com>
2016-10-15 20:07:46 +0530

    Modified Section.java

--- BucketFill/src/com/bfa/model/Section.java
+++ BucketFill/src/com/bfa/model/Section.java
@@ -46,6 +46,7 @@ public class Section implements DBConnection{
     
     private int teacherID, semester;
     private String subject, name;
+    public static int TOTAL_NUMBER_OF_SECTIONS;
     
     /**
      *
@@ -131,5 +132,58 @@ public class Section implements DBConnection{
             System.out.println("Error in section by year "+e);
             return null;
         }
-    }  
+    } 
+    
+    public static void setTotalNumberOfSections(){
+        for(int i=2; i<=4; i++)
+            TOTAL_NUMBER_OF_SECTIONS += Section.getNumberOfSectionsByYear(i);
+    }
+    
+    /**
+     *
+     * @param year
+     * @return number of sections in a year as integer
+     */
+    public static int getNumberOfSectionsByYear(int year){
+        Connection myConnection = DBConnection.createConnection();
+        ResultSet rs = null;
+        
+        try{
+            
+            PreparedStatement myPreStatement = myConnection.prepareStatement("SELECT COUNT(DISTINCT Name) FROM Section WHERE Name NOT IN (SELECT Name FROM Section WHERE Name = 1) AND (Semester = ? OR Semester = ?)");
+            switch(year)
+            {
+                case 2: myPreStatement.setInt(1, 3);
+                        myPreStatement.setInt(2, 4);
+                        rs = myPreStatement.executeQuery();
+                        break;
+                        
+                case 3: myPreStatement.setInt(1, 5);
+                        myPreStatement.setInt(2, 6);
+                        rs = myPreStatement.executeQuery();
+                        break;
+                        
+                case 4: myPreStatement.setInt(1, 7);
+                        myPreStatement.setInt(2, 8);
+                        rs = myPreStatement.executeQuery();
+                        break;
+                        
+                default: myPreStatement.setInt(1, 1);
+                         myPreStatement.setInt(2, 2);
+                         rs = myPreStatement.executeQuery();
+            }
+            
+            int noOfSections = 0;
+            while(rs.next())
+            {
+                noOfSections = rs.getInt(1);
+            }
+            myConnection.close();
+            return noOfSections;
+        }
+        catch(Exception e){
+            System.out.println("Error in section by year "+e);
+            return 0;
+        }
+    }
 }
